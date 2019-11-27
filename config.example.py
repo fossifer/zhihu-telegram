73385f5ad3920975aae87806dccfabde6f7fae8b
lziad <23732689+lziad@users.noreply.github.com>
2019-11-27 23:26:05 +0800

    +config example

---
+++ config.example.py
@@ -0,0 +1,21 @@
+debug = False
+username = '知乎用户名'
+password = '知乎密码'
+# 日志所在文件夹
+log_dir = './tmp'
+# 知乎登录的 cookie 所在路径
+cookie_file = './tmp/cookies.txt'
+
+# 这个可以从阅读该人私信时的 URL 得到，是长度为 32 的一串十六进制数
+target_hash = '1234567890abcdef1234567890abcdef'
+
+# 登录 Telegram 机器人用的代理，目前仅支持 SOCKS 代理
+proxy = True
+proxy_address = '127.0.0.1'
+proxy_port = 1081
+
+# Telegram 机器人的 token，找 BotFather 申请
+token = '114514810:ABcdefghijklmnOPq-rstuvwxyz'
+# 你申请上述机器人的账号的 api_id 和 api_hash，在 https://my.telegram.org 申请
+api_id = 123456
+api_hash = '1234567890abcdef1234567890abcdef'
